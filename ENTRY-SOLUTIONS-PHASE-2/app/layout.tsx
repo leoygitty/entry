d9415e173aa96be0d27f2e2adc77998ceec56903
leoygitty <haydenyori11@gmail.com>
2025-12-18 23:41:08 -0500

    Update layout.tsx

--- ENTRY-SOLUTIONS-PHASE-2/app/layout.tsx
+++ ENTRY-SOLUTIONS-PHASE-2/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import MobileCallBar from "@/components/MobileCallBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,10 +10,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={`${inter.className} bg-background`}>
-        <Header />
+         <Header />
         {children}
         <Footer />
       </body>
+      <MobileCallBar />
     </html>
   );
 }
