af2510140ee927847ef7d72f1f85f51714b47343
leoygitty <haydenyori11@gmail.com>
2025-12-19 15:26:39 -0500

    Update layout.tsx

--- ENTRY-SOLUTIONS-PHASE-2/app/layout.tsx
+++ ENTRY-SOLUTIONS-PHASE-2/app/layout.tsx
@@ -13,7 +13,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {children}
         <Footer />
       </body>
-      <MobileCallBar />
     </html>
   );
 }
