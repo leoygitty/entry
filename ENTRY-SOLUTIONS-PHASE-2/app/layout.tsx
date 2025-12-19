1556d9adabbbc2fdb059b55afcd834a9ec559c84
leoygitty <haydenyori11@gmail.com>
2025-12-19 15:25:40 -0500

    Update layout.tsx

--- ENTRY-SOLUTIONS-PHASE-2/app/layout.tsx
+++ ENTRY-SOLUTIONS-PHASE-2/app/layout.tsx
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import MobileCallBar from "@/components/MobileCallBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
