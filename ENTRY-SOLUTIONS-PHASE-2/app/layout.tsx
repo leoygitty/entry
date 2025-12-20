cca5bea8005765416ed6443a304c19852b10b374
leoygitty <haydenyori11@gmail.com>
2025-12-20 14:03:20 -0500

    Update layout.tsx

--- ENTRY-SOLUTIONS-PHASE-2/app/layout.tsx
+++ ENTRY-SOLUTIONS-PHASE-2/app/layout.tsx
@@ -1,17 +1,28 @@
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-background`}>
-         <Header />
+        <Header />
         {children}
         <Footer />
+
+        {/* Calendly global widget */}
+        <Script
+          src="https://assets.calendly.com/assets/external/widget.js"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
