cadb6bc6503934e9b658b5b604db8da52a876811
leoygitty <haydenyori11@gmail.com>
2025-12-21 01:16:23 -0500

    Update layout.tsx

--- ENTRY-SOLUTIONS-PHASE-2/app/layout.tsx
+++ ENTRY-SOLUTIONS-PHASE-2/app/layout.tsx
@@ -13,6 +13,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* CallRail Dynamic Number Insertion */}
+        <Script
+          id="callrail"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                var cr = document.createElement("script");
+                cr.type = "text/javascript";
+                cr.async = true;
+                cr.src = <script type="text/javascript" src="//cdn.callrail.com/companies/554986649/e2933efaed1eccbd3c90/12/swap.js"></script> ;
+                var s = document.getElementsByTagName("script")[0];
+                s.parentNode.insertBefore(cr, s);
+              })();
+            `,
+          }}
+        />
+      </head>
+
       <body className={`${inter.className} bg-background`}>
         <Header />
         {children}
