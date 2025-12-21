710b7363b4568b87d0bcc2389167a92c2f0b8f2b
leoygitty <haydenyori11@gmail.com>
2025-12-20 21:51:07 -0500

    Create ProjectIcon.tsx

---
+++ ENTRY-SOLUTIONS-PHASE-2/components/icons/ProjectIcon.tsx
@@ -0,0 +1,10 @@
+export function ProjectIcon({ className = "" }: { className?: string }) {
+  return (
+    <img
+      src="/icons/project-house.svg"
+      alt=""
+      aria-hidden="true"
+      className={className}
+    />
+  );
+}
