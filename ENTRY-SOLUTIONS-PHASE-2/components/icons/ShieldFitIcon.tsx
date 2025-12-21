c6accd32d855e706678f85d4751b7901486df32d
leoygitty <haydenyori11@gmail.com>
2025-12-20 21:14:28 -0500

    Update ShieldFitIcon.tsx

--- ENTRY-SOLUTIONS-PHASE-2/components/icons/ShieldFitIcon.tsx
+++ ENTRY-SOLUTIONS-PHASE-2/components/icons/ShieldFitIcon.tsx
@@ -1,23 +1,10 @@
 export function ShieldFitIcon({ className = "" }: { className?: string }) {
   return (
-    <svg
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
+    <img
+      src="/icons/shield-fit.svg"
+      alt=""
       aria-hidden="true"
-      dangerouslySetInnerHTML={{
-        __html: `
-<!-- BEGIN exact shield_red_white_blue.svg -->
-${require("fs")
-  ? "" 
-  : `
-<g>
-  <!-- SVG content injected exactly as-is -->
-</g>
-`}
-<!-- END exact shield_red_white_blue.svg -->
-        `,
-      }}
+      className={className}
     />
   );
 }
